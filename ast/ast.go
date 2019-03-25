614f242c0415fda750b77ffb1935d92bdfd77508
Di Wen <ifyouseewendy@gmail.com>
2019-03-24 21:33:39 -0400

    Add String method for all AST node

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,9 @@
 package ast
 
-import "donkey/token"
+import (
+	"bytes"
+	"donkey/token"
+)
 
 // Node is an interal presentation of a node on AST. Every node in our AST has
 // to implement the Node interface, meaning it has to provide a TokenLiteral()
@@ -11,6 +14,7 @@ import "donkey/token"
 // and some the Expression interface.
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement is the meta unit of a program. Every node implements it has to provide a
@@ -39,6 +43,16 @@ func (p *Program) TokenLiteral() string {
 	return ""
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // LetStatement is one of the three types of statements.
 // Form: TOKEN NAME = VALUE
 type LetStatement struct {
@@ -52,6 +66,22 @@ func (ls *LetStatement) statementNode() {}
 // TokenLiteral is a Node implementation for LetStatement
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 // ReturnStatement is one of the three types of statements.
 // Form: TOKEN ReturnValue
 type ReturnStatement struct {
@@ -63,6 +93,40 @@ func (rs *ReturnStatement) statementNode() {}
 
 // TokenLiteral is a Node implementation for ReturnStatement
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// ExpressionStatement is a wrapper over Expression, thus we can add it to the
+// Statements slice of ast.Program.
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+// TokenLiteral is a Node implementation for ExpressionStatement
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
 
 // Identifier as part of LetStatement, also implements the Expression interface,
 // as in other part of Monkey language, it produce values like let x =
@@ -76,3 +140,7 @@ func (i *Identifier) expressionNode() {}
 
 // TokenLiteral is a Node implementation for Identifier
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (i *Identifier) String() string {
+	return i.Value
+}
