dd5d2d71458ca45ceb68e36ec0c229f39c4d8423
Di Wen <ifyouseewendy@gmail.com>
2019-04-05 14:27:36 -0400

    2-9 Use parser in REPL

--- src/donkey/repl/repl.go
+++ src/donkey/repl/repl.go
@@ -3,13 +3,15 @@ package repl
 import (
 	"bufio"
 	"donkey/lexer"
-	"donkey/token"
+	"donkey/parser"
 	"fmt"
 	"io"
 )
 
+// PROMPT is the prompt character for REPL
 const PROMPT = ">> "
 
+// Start is the entry for REPL
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
@@ -22,9 +24,48 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+// DONKEY_FACE is used in REPL for friendly compilation error
+const DONKEY_FACE = `
+                          /\          /\
+                         ( \\        // )
+                          \ \\      // /
+                           \_\\||||//_/
+                            \/ _  _ \
+                           \/|(O)(O)|
+                          \/ |      |
+      ___________________\/  \      /
+     //                //     |____|
+    //                ||     /      \
+   //|                \|     \ 0  0 /
+  // \       )         V    / \____/
+ //   \     /        (     /
+""     \   /_________|  |_/
+       /  /\   /     |  ||
+      /  / /  /      \  ||
+      | |  | |        | ||
+      | |  | |        | ||
+      |_|  |_|        |_||
+       \_\  \_\        \_\\ Hard'96
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, DONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some donkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
